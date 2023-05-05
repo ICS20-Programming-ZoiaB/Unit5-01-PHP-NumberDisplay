9eca3036ae66c3afe17e740ce0f261533cb467ef
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-05 14:45:25 +0000

    fixed indentation

--- results.php
+++ results.php
@@ -13,23 +13,24 @@ $counter = $minNumber;
 
 // If statement to ensure that maximum is greater than minumum.
 if ($minNumber > $maxNumber) {
-  $message = "Ensure that minimum value is less than or equal to maximum value.";
+$message = "Ensure that minimum value is less than or equal to maximum value.";
 }
-  
+
 // Else, make list of numbers
 else {
-  // While loop that provides the range
-  while (($counter >= $minNumber) && ($counter <= $maxNumber)) {
-    
-    // List of numbers with a break
-    $numbers = $numbers . $counter . "<br>";
+  
+// While loop that provides the range
+while (($counter >= $minNumber) && ($counter <= $maxNumber)) {
 
-    // Give an increment for counter
-    $counter++;
+// List of numbers with a break
+$numbers = $numbers . $counter . "<br>";
 
-    // Set the message variable to display results
-    $message = "Here are all the numbers between the minimum and maximum numbers that you entered:<br><br>" . $numbers;
-  }
+// Give an increment for counter
+$counter++;
+
+// Set the message variable to display results
+$message = "Here are all the numbers between the minimum and maximum numbers that you entered:<br><br>" . $numbers;
+}
 }
 
 // Displaying the results (the range of numbers) back to the user
